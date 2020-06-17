a678cc5d348e18743bbb33b69f393506aafa3a3b
WildCodeSchool <anelisynagano@gmail.com>
2020-06-17 21:12:33 +0100

    Updates

---
+++ precache-manifest.25711c90f47ee4de5a80959fa5252f58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e67cedf68341bd77093317f278311ba",
+    "url": "/react-intro/index.html"
+  },
+  {
+    "revision": "085b8cbc7aff21a5f973",
+    "url": "/react-intro/static/css/main.4f661cf4.chunk.css"
+  },
+  {
+    "revision": "a82c789e4a99d29bd732",
+    "url": "/react-intro/static/js/2.57cf4e78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-intro/static/js/2.57cf4e78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "085b8cbc7aff21a5f973",
+    "url": "/react-intro/static/js/main.632c767a.chunk.js"
+  },
+  {
+    "revision": "bbabea2acd9d0d43c754",
+    "url": "/react-intro/static/js/runtime-main.4a178d3f.js"
+  }
+]);
